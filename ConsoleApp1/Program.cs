6d37db895cef0d527a262f596393809bd9e47170
Stepan <finlandcowboy@gmail.com>
2020-05-07 18:31:32 +0300

    Господь БОГ ДАЛ МНЕ СИЛ ЧТОБЫ Я 4 ЁБАНЫХ ЧАСА ПАРИЛСЯ С ПАРСЕРОМ ЁБАНОГО ДЖСОНА СПАСИБО БОГАМ ВЕЛИКИМ

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,47 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Newtonsoft.Json;
 using telegram;
 
 namespace ConsoleApp1
 {
+
+
+
+
     class Program
     {
+
+
         static Dictionary<string, string> Questions;
+        static public List<Dictionary<string, string>> Quotes;
 
         static void Main(string[] args)
         {
 
-            var Data = System.IO.File.ReadAllText(@"C:\Users\User\source\repos\ConsoleApp1\text.json", System.Text.Encoding.UTF8);
+            var Data = System.IO.File.ReadAllText(@"C:\Users\User\Documents\programming\Telegram-BOT-HELPER\text.json", System.Text.Encoding.UTF8);
+            var Quotes_data = System.IO.File.ReadAllText(@"C:\Users\User\Documents\programming\Telegram-BOT-HELPER\quotes.json", System.Text.Encoding.UTF8);
             Questions = JsonConvert.DeserializeObject<Dictionary<string, string>>(Data);
-
+            Quotes = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(Quotes_data);
             
+
+
             var API = new TelegramApi();
 
             while (true)
             {
                 var Updates = API.GetUpdates();
-               
+
                 foreach (var update in Updates)
                 {
-                   var Question = update.message.text;
-                   var Answer = AnswerQuestion(Question);
-                   API.sendMessage(Answer, update.message.chat.id);
+                    var Question = update.message.text;
+                    var Answer = AnswerQuestion(Question);
+                    API.sendMessage(Answer, update.message.chat.id);
                 }
-                
+
             }
             Console.ReadKey();
 
-        }        
-        
-        
+        }
+
+
         static string AnswerQuestion(string UserQuestion)
         {
             UserQuestion = UserQuestion.ToLower();
             List<string> Answers = new List<string>();
-                       
+
             foreach (var Question in Questions)
             {
                 if (UserQuestion.Contains(Question.Key))
@@ -51,8 +60,21 @@ namespace ConsoleApp1
                     Answers.Add(Question.Value);
                 }
             }
-                 
-            if(Answers.Count == 0)
+
+
+            if (UserQuestion.StartsWith("фраза"))
+            {
+                Random rnd = new Random();
+                int value = rnd.Next() % 5000;
+                if (Quotes[value].ContainsKey("quoteText") && Quotes[value].ContainsKey("quoteAuthor"))
+                {
+                    Answers.Add( Quotes[value]["quoteAuthor"] + "\n" + Quotes[value]["quoteText"]);
+                }
+            }
+
+
+
+            if (Answers.Count == 0)
             {
                 Answers.Add("Я тебя не понимаю (");
             }
@@ -71,23 +93,15 @@ namespace ConsoleApp1
                 Answers.Add(Headline);
             }
 
-/*
-            if(UserQuestion.Contains("фраза") || UserQuestion.Contains("цитата") || UserQuestion.Contains("сказано")){
-                string quote = "фраза";
-                var QuotesApi = new Quotes();
-                var Line = QuotesApi.GetQuote(quote);
-                Answers.Add(Line);
-            }
-            */
-      /*      if (UserQuestion.StartsWith("какая погода в городе"))
-            {
-                var words = UserQuestion.Split(' ');
-                var City = words[words.Length - 1];
+            /*      if (UserQuestion.StartsWith("какая погода в городе"))
+                  {
+                      var words = UserQuestion.Split(' ');
+                      var City = words[words.Length - 1];
 
-                var WeatherApi = new Weather();
-                var Forecast = WeatherApi.getWeatherInCity(City);
-                Answers.Add(Forecast);
-            }*/
+                      var WeatherApi = new Weather();
+                      var Forecast = WeatherApi.getWeatherInCity(City);
+                      Answers.Add(Forecast);
+                  }*/
 
 
             if (UserQuestion.Contains("какой сегодня день"))
@@ -96,8 +110,8 @@ namespace ConsoleApp1
                 Answers.Add($"Точное время: {Date}");
             }
 
-             return String.Join(", ", Answers);
-                      
+            return String.Join(", ", Answers);
+
         }
     }
 }
